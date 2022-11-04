aeeb1b82c397f93f4e4d0a4a9d002082f659bd74
FlyingFox1216 <70279596+flying-fox1216@users.noreply.github.com>
2022-11-04 16:04:12 +0900

    폴리곤 수정

--- matter.js
+++ matter.js
@@ -10,18 +10,7 @@ let boxes = [];
 let ground;
 let MouseConstraint = Matter.MouseConstraint,
   Mouse = Matter.Mouse;
-
-//   var mouse = Mouse.create(render.canvas),
-//   mouseConstraint = MouseConstraint.create(engine, {
-//     mouse: mouse,
-//     constraint: {
-//       stiffness: 0.2,
-//       render: {
-//         visible: false,
-//       },
-//     },
-//   });
-// Composite.add(world, mouseConstraint);
+let sides = [2, 3, 4, 5, 6, 7, 8, 9];
 
 function setup() {
   createCanvas(800, 600);
@@ -30,7 +19,10 @@ function setup() {
   ground2 = new Rect(650, 610, 300, 60, "#C0AAA9", { isStatic: true });
   wall_1 = new Rect(0, 150, 20, 900, "#C0AAA9", { isStatic: true });
   wall_2 = new Rect(800, 150, 20, 900, "#C0AAA9", { isStatic: true });
-  obj = new Polygon(width / 2, 400, 3, 100, "#C0AAA9", { isStatic: true });
+  obj = new Polygon(width / 2, 400, 3, 100, "#C0AAA9", {
+    isStatic: true,
+    angle: Math.PI * -1.5,
+  });
   // new Rect(400, 350, 150, 100, "#C0AAA9", { isStatic: true });
 
   // obj_1 = new Rect(800, 150, 20, 900, "#C0AAA9", { isStatic: true });
@@ -39,31 +31,50 @@ function setup() {
   Composite.add(engine.world, wall_1.bodies);
   Composite.add(engine.world, wall_2.bodies);
   Composite.add(engine.world, obj.bodies);
-  // Composite.add(engine.world, obj_1.bodies);
 
   // Matter.Runner.run(engine);
   // Render.run(Render);
 }
 
 function mousePressed() {
-  let angle = random(0, 6);
+  let angle = random(sides);
   let size = random(10, 40);
   let randomColor = color(random(256), random(256), random(256));
   let newRect;
-  if (mouseButton === LEFT) {
-    // newRect = new Rect(mouseX, mouseY, size, size, randomColor);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-  } else if (mouseButton === CENTER) {
-    print(angle);
-    if (angle >= 3) {
-      newRect = new Polygon(mouseX, mouseY, angle, size, randomColor);
+  // if (mouseButton === LEFT) {
+  // } else
+  if (mouseButton === CENTER) {
+    if (angle > 2) {
+      newRect = new Polygon(mouseX, mouseY, angle, size, randomColor, {
+        restitution: 1,
+      });
     } else if (angle < 3) {
       newRect = new Circle(mouseX, mouseY, size, randomColor);
     }
   }
   Composite.add(engine.world, newRect.bodies);
+  // Composite.add(engine.world, newRect2.body);
+  boxes.push(newRect); // 묶음 boxes에 사각형 추가, 아래 render 코드에 사각형을 그릴수 있도록 함
+}
+
+function mouseDragged() {
+  let angle = random(sides);
+  let size = random(10, 40);
+  let randomColor = color(random(256), random(256), random(256));
+  let newRect;
+  // if (mouseButton === LEFT) {
+  // } else
+  if (mouseButton === CENTER) {
+    if (angle > 2) {
+      newRect = new Polygon(mouseX, mouseY, angle, size, randomColor, {
+        restitution: 1,
+      });
+    } else if (angle < 3) {
+      newRect = new Circle(mouseX, mouseY, size, randomColor);
+    }
+  }
+  Composite.add(engine.world, newRect.bodies);
+  // Composite.add(engine.world, newRect2.body);
   boxes.push(newRect); // 묶음 boxes에 사각형 추가, 아래 render 코드에 사각형을 그릴수 있도록 함
 }
 
@@ -82,6 +93,7 @@ function draw() {
   ground2.render();
   wall_1.render();
   wall_2.render();
-  // obj_1.render();
   obj.render();
 }
+
+// 해야할 거 = 마우스 드래그, 아래로 떨어졌을 시 위로 다시 나오게하기
--- polygon.js
+++ polygon.js
@@ -1,21 +1,49 @@
+// class Polygon {
+//   constructor(x, y, s, r, c, opt) {
+//     this.s = s; //side
+//     this.r = r; //width = radius
+//     this.c = c;
+//     var opt = {
+//       restitution: 1,
+//     };
+//     this.bodies = Bodies.polygon(x, y, this.s, this.r, opt);
+//   }
+
+//   render() {
+//     rectMode(CENTER);
+//     push();
+//     fill(this.c);
+//     translate(this.bodies.position.x, this.bodies.position.y);
+//     rotate(this.bodies.angle);
+//     polygon(0, 0, this.r, this.s);
+//     pop();
+//   }
+// }
+
 class Polygon {
   constructor(x, y, s, r, c, opt) {
-    this.s = s; //side
-    this.r = r; //width = radius
+    this.sides = s;
+    this.radius = r;
     this.c = c;
-    var opt = {
-      restitution: 1,
-    };
-    this.bodies = Bodies.polygon(x, y, this.s, this.r, opt);
-  }
 
-  render() {
-    rectMode(CENTER);
-    push();
-    fill(this.c);
-    translate(this.bodies.position.x, this.bodies.position.y);
-    rotate(this.bodies.angle);
-    rect(0, 0, this.s, this.r);
-    pop();
+    this.bodies = Matter.Bodies.polygon(x, y, this.sides, this.radius, opt);
+    // Matter.Composite.add(engine.world, this.body);
   }
+  render() {
+    fill(this.c);
+
+    beginShape();
+    this.bodies.vertices.forEach((v) => {
+      vertex(v.x, v.y);
+    });
+    endShape(CLOSE);
+  }
+  // renderDirVector() {
+  //   line(
+  //     this.body.position.x,
+  //     this.body.position.y,
+  //     this.body.vertices[0].x,
+  //     this.body.vertices[0].y
+  //   );
+  // }
 }
